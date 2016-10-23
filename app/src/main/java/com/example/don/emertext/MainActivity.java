b288ca37242410d3a7eb4fb31af63dfb960d4a1b
gerard <gbonner689@gmail.com>
2016-10-23 12:53:45 +0100

    request permissions for SMS implemented

--- app/src/main/java/com/example/don/emertext/MainActivity.java
+++ app/src/main/java/com/example/don/emertext/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.don.emertext;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +17,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        }
+
+
+    public boolean checkSMS(View view){
+        boolean permissionGranted = ContextCompat.checkSelfPermission(this,Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED;
+        if (permissionGranted) {
+            return true;
+        }
+        else
+        {  return false;
+        }
+    }
+
+    public boolean requestSMS(View view){
+        if (checkSMS(view)){
+            return true;
+        }
+        else{
+            //try to get permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    6);
+            if (checkSMS(view)) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
     }
 
     public void enterDetails(View view){
-        Intent intent = new Intent(this, DetailsForm.class);
-        startActivity(intent);
-    }
-}
+        //Below is a small chunk of code which checks if the user granted the SMS permission or not
+            if (requestSMS(view)){
+            Intent intent = new Intent(this, DetailsForm.class);
+            startActivity(intent);}
+        else {
+                Button button = (Button) findViewById(R.id.button);
+                button.setText("You haven't granted SMS permissions");
+            }
+
+}}
