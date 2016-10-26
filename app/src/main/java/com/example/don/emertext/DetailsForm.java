94ab2e16b24f81aef4e87c45ac686507390bd7a7
gerard <gbonner689@gmail.com>
2016-10-26 12:52:11 +0100

    slight cleanup

--- app/src/main/java/com/example/don/emertext/DetailsForm.java
+++ app/src/main/java/com/example/don/emertext/DetailsForm.java
@@ -1,18 +1,35 @@
 package com.example.don.emertext;
 
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.net.HttpURLConnection;
+
+
 public class DetailsForm extends Activity {
+
+    private boolean overrideNumber = false;
+    private String EMERGENCY_NUMBER = "0831453528";
+    private int overrideCounter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,28 +37,69 @@ public class DetailsForm extends Activity {
         setContentView(R.layout.activity_details_form);
 
         //Set the contents for the county selector
-    Spinner spinner = (Spinner) findViewById(R.id.county_select_spinner);
-    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        Spinner spinner = (Spinner) findViewById(R.id.county_select_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.county_list, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+
+
     }
 
-    public void sendSMS(View view)
-    {
+
+
+
+    public String composeMessage(View view) {
+        EditText firstname = (EditText) findViewById(R.id.fnameInput);
+        EditText lastname = (EditText) findViewById(R.id.lnamepinput);
+        EditText address1 = (EditText) findViewById(R.id.address1);
+        EditText address2 = (EditText) findViewById(R.id.address2);
+
+        String message = firstname.getText().toString() + " " +
+                lastname.getText().toString() + " " +
+                address1.getText().toString() + " " +
+                address2.getText().toString() + " " +
+                "requires an ambulance.";
+        return message;
+    }
+
+
+    public void checkNetwork(View view) {
+        TelephonyManager tel = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String networkOperator = tel.getNetworkOperator();
+        if (networkOperator.equals("27203")) {
+
+        }
+    }
+
+    public void revealOverride(View view) {
+        if (++overrideCounter >= 5) {
+            (findViewById(R.id.overrideNumber)).setVisibility(View.VISIBLE);
+            overrideNumber = true;
+        }
+    }
+
+    public void sendSMS(View view) {
+
         SmsManager text = SmsManager.getDefault();
-        String number = "0831453528";  // The number on which you want to send SMS
-        String message = "";
-        Intent resultIntent= new Intent(this,MainActivity.class);
-        PendingIntent intent=PendingIntent.getActivity(this,
+        String number;
+        if (overrideNumber) {
+            number = ((EditText) findViewById(R.id.overrideNumber)).getText().toString();
+        } else {
+            number = EMERGENCY_NUMBER;
+        }
+        String message = composeMessage(view);
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent intent = PendingIntent.getActivity(this,
                 0,
                 resultIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
         text.sendTextMessage(number // Number to send to
-                ,null               // Message centre to send to (we'll never want to change this)
-                ,message            // Message to send
-                ,null               // The PendingIntent to perform when the message is successfully sent
-                ,intent);           // The PendingIntent to perform when the message is successfully delivered
+                , null               // Message centre to send to (we'll never want to change this)
+                , message            // Message to send
+                , null               // The PendingIntent to perform when the message is successfully sent
+                , intent);           // The PendingIntent to perform when the message is successfully delivered
         TextView textView = (TextView) findViewById(R.id.return_message);
         textView.setVisibility(view.VISIBLE);
     }
--- app/src/main/java/com/example/don/emertext/MainActivity.java
+++ app/src/main/java/com/example/don/emertext/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.don.emertext;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+
 
 //  Function to just check if we have SEND_SMS permission
     public boolean checkSMS(View view){
