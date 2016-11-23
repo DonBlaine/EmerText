e110f157da6c8b45f1dccc3008321acb3dd44f62
gerard <gbonner689@gmail.com>
2016-11-23 12:42:40 +0000

    Merge UnsupportedNetwork into MainActivity

--- app/src/main/java/com/example/don/emertext/MainActivity.java
+++ app/src/main/java/com/example/don/emertext/MainActivity.java
@@ -12,24 +12,36 @@ import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView network_text;
+    ImageView network_image;
+    TextView sim_text;
+    ImageView sim_image;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        View view=getCurrentFocus();
-        checkNetwork(view);
         SharedPreferences sharedPref = getSharedPreferences(
                 getString(R.string.personal_details_file), Context.MODE_PRIVATE);
 
+
+        network_text = (TextView) findViewById(R.id.net_status_text);
+        network_image = (ImageView) findViewById(R.id.network_icon);
+        sim_text = (TextView) findViewById(R.id.sim_status_text);
+        sim_image = (ImageView) findViewById(R.id.sim_icon);
+        checkNetwork();
+        checkSim();
+
+
+        if (checkNetwork() && checkSim()) {
         //If never initialised redirect to details edit screen
         if (!sharedPref.getBoolean(getString(R.string.details_initialised_key),false)){
             Intent intent=new Intent(this, TabbedDetails.class);
@@ -46,8 +58,73 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         }
+        }
 
 
+    public int operatorCode(String mnc) {
+        int value = 0;
+        String[] operators = {"27201", "27202", "27203", "27205"};
+        for (int index = 0; index < operators.length; index++) {
+            if (mnc.equals(operators[index])) {
+                value = index;
+            }
+        }
+        //Network codes 27205 and 27202 are both for Three IE, so return the same value
+        if (value == 3) {
+            value = 1;
+        }
+        return value;
+    }
+
+    public boolean checkNetwork() {
+        TelephonyManager tel = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String networkOperator = tel.getNetworkOperator();
+        // String sim = tel.getSimOperator();
+        boolean isSupported = false;
+        if (operatorCode(networkOperator) > 0) {
+            isSupported = true;
+        }
+        // if (!sim.equals(networkOperator)){
+        //      if (!(sim.equals("27202") || sim.equals("27205")) && (networkOperator.equals("27202") || networkOperator.equals("27205"))){
+        //         isSupported=false;
+        //     }
+
+
+        if (isSupported) {
+            network_text.setText("Connected network should be supported. (" + networkOperator + ")");
+            network_image.setImageDrawable(getDrawable(R.drawable.green_check));
+        } else {
+            network_text.setText("Connected network is not supported. (" + networkOperator + ")");
+            network_image.setImageDrawable(getDrawable(R.drawable.red_block));
+
+        }
+        return isSupported;
+    }
+
+    public boolean checkSim() {
+        TelephonyManager tel = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String networkOperator = tel.getSimOperator();
+        // String sim = tel.getSimOperator();
+        boolean isSupported = false;
+        if (operatorCode(networkOperator) > 0) {
+            isSupported = true;
+        }
+        // if (!sim.equals(networkOperator)){
+        //      if (!(sim.equals("27202") || sim.equals("27205")) && (networkOperator.equals("27202") || networkOperator.equals("27205"))){
+        //         isSupported=false;
+        //     }
+
+
+        if (isSupported) {
+            sim_text.setText("SIM network should be supported. (" + networkOperator + ")");
+            sim_image.setImageDrawable(getDrawable(R.drawable.green_check));
+        } else {
+            sim_text.setText("SIM network is not supported. (" + networkOperator + ")");
+            sim_image.setImageDrawable(getDrawable(R.drawable.red_block));
+
+        }
+        return isSupported;
+    }
 //  Function to just check if we have SEND_SMS permission
     public boolean checkSMS(View view){
         boolean permissionGranted = ContextCompat.checkSelfPermission(this,Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED;
@@ -59,36 +136,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void checkNetwork(View view){
-        String[][] SUPPORTED_OPERATORS = {{"27201","Vodafone IE"},{"27202","3 IE"},{"27203", "Meteor"},{"27205","Three IE"}};
-        TelephonyManager tel = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        String networkOperator = tel.getNetworkOperator();
-        String sim = tel.getSimOperator();
-        boolean isSupported=false;
-        int location=-1;
-        for (int i=0;i<SUPPORTED_OPERATORS.length;i++){
-            if (SUPPORTED_OPERATORS[i][0].equals(networkOperator)) {
-                isSupported=true;
-                location=i;}
-        }
-        if (!sim.equals(networkOperator)){
-            if (!(sim.equals("27202") || sim.equals("27205")) && (networkOperator.equals("27202") || networkOperator.equals("27205"))){
-                isSupported=false;
-            }
-
-        }
-        if (isSupported){
-        Context context = getApplicationContext();
-        CharSequence text = "You appear to be using " + SUPPORTED_OPERATORS[location][1] + ". This service should be supported";
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-    }
-    else {
-            Intent intent = new Intent(this, UnsupportedNetwork.class);
-            startActivity(intent);
-        }}
 
 // Function to check if we have SEND_SMS and request it if we don't.
     public boolean requestSMS(View view){
@@ -111,7 +158,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void enterDetails(View view){
             if (requestSMS(view)){
-            Intent intent = new Intent(this, UnsupportedNetwork.class);
+                Intent intent = new Intent(this, TabbedDetails.class);
             startActivity(intent);}
         else {
                 Button button = (Button) findViewById(R.id.button);
