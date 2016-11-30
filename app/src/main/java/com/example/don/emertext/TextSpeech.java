6501bfa5cc2b116741218c4ace1915d7892c1578
akhilesh <akhilesh.sharma@ucdconnect.ie>
2016-11-30 13:49:03 +0000

    getting previous version of text speech code

--- app/src/main/java/com/example/don/emertext/TextSpeech.java
+++ app/src/main/java/com/example/don/emertext/TextSpeech.java
@@ -1,18 +1,14 @@
 package com.example.don.emertext;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,18 +18,16 @@ public class TextSpeech extends AppCompatActivity {
 
     TextToSpeech ts;
     String toSpeak;
-    ScrollView scroll;
-    EditText writable;
-    int i;
+    TextView helperText;
+    TextView speakText;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_speech);
-        scroll = (ScrollView) findViewById(R.id.helperTextMessage);
-        writable = (EditText)findViewById(R.id.editText);
-        i=0;
+        helperText = (TextView) findViewById(R.id.helperTextMessage);
+        speakText = (TextView) findViewById(R.id.saidMessage);
 
     }
 
@@ -49,49 +43,18 @@ public class TextSpeech extends AppCompatActivity {
         });
         defineButtons();
 
-
-
-        (writable).setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        ((EditText)findViewById(R.id.editText)).setOnEditorActionListener(new TextView.OnEditorActionListener() {
 
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
 
                 if  ((actionId == EditorInfo.IME_ACTION_DONE)) {
 
-                    if (!writable.getText().equals("")) {
-                        showMessage();
-                    }
+                    helperText.setText(((EditText) findViewById(R.id.editText)).getText());
                 }
 
                 return false;
             }
         });
-    }
-
-
-    public void showMessage(){
-
-        i+=1;
-        LinearLayout ll1 = (LinearLayout)findViewById(R.id.textexchange);
-
-        TextView nmsg = new TextView(this);
-        nmsg.setText(writable.getText());
-
-        if(i%2==0) {
-            nmsg.setBackgroundResource(R.drawable.message_received_wrap);
-        }
-        else {
-            nmsg.setBackgroundResource(R.drawable.message_wrap);
-        }
-        nmsg.setTextColor(Color.BLACK);
-        LinearLayout.LayoutParams prop = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        prop.setMargins(0,0,0,8);
-
-        nmsg.setLayoutParams(prop);
-        nmsg.setPadding(5,5,5,5);
-        nmsg.setTextSize(18);
-
-        ll1.addView(nmsg);
-        writable.setText("");
     }
 
     protected void onRestart(){
@@ -123,7 +86,6 @@ public class TextSpeech extends AppCompatActivity {
         findViewById(R.id.button12).setOnClickListener(buttonClickListener);
         findViewById(R.id.button13).setOnClickListener(buttonClickListener);
         findViewById(R.id.button14).setOnClickListener(buttonClickListener);
-
 
         findViewById(R.id.agree).setOnClickListener(buttonClickListener);
         findViewById(R.id.decline).setOnClickListener(buttonClickListener);
@@ -184,6 +146,7 @@ public class TextSpeech extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
         ts.speak(toSpeak,TextToSpeech.QUEUE_FLUSH,null);
     }
+
 
 
 
