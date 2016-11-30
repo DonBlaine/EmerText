a8148bf5cd4baa68a704ab4c1109126c378f998c
akhilesh <akhilesh.sharma@ucdconnect.ie>
2016-11-30 17:15:00 +0000

    copying from the tutorial to receive sms

--- app/src/main/java/com/example/don/emertext/SmsReceiver.java
+++ app/src/main/java/com/example/don/emertext/SmsReceiver.java
@@ -3,66 +3,35 @@ package com.example.don.emertext;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.PhoneNumberUtils;
-import android.telephony.SmsMessage;
-import android.view.Gravity;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.telephony.gsm.SmsMessage;
 import android.widget.Toast;
 
-public class SmsReceiver extends BroadcastReceiver {
-    private Bundle bundle;
-    private SmsMessage currentSMS;
-    private String sender;
-    private String message;
+// taken from mobiforge
 
+public class SmsReceiver extends BroadcastReceiver
+{
     @Override
-    public void onReceive(Context context, Intent intent) {
-
-        if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
-            bundle = intent.getExtras();
-            if (bundle != null) {
-                Object[] pdu_Objects = (Object[]) bundle.get("pdus");
-                if (pdu_Objects != null) {
-
-                    for (Object aObject : pdu_Objects) {
-
-                        currentSMS = getIncomingMessage(aObject, bundle);
-
-                        sender = currentSMS.getDisplayOriginatingAddress();
-                        message = currentSMS.getDisplayMessageBody().toString();
-
-                        if(PhoneNumberUtils.compare(sender, "0868303866")) {
-
-                            message = message.trim();
-                            if (!message.equals("")) {
-                                Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
-                            }
-
-                        }
-
-                    }
-                }
+    public void onReceive(Context context, Intent intent)
+    {
+        //---get the SMS message passed in---
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        String str = "";
+        if (bundle != null)
+        {
+            //---retrieve the SMS message received---
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for (int i=0; i<msgs.length; i++){
+                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                str += "SMS from " + msgs[i].getOriginatingAddress();
+                str += " :";
+                str += msgs[i].getMessageBody().toString();
+                str += "n";
             }
+            //---display the new SMS message---
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
         }
     }
-
-
-    private SmsMessage getIncomingMessage(Object aObject, Bundle bundle) {
-        SmsMessage currentSMS;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            String format = bundle.getString("format");
-            currentSMS = SmsMessage.createFromPdu((byte[]) aObject, format);
-        } else {
-            currentSMS = SmsMessage.createFromPdu((byte[]) aObject);
-        }
-        return currentSMS;
-    }
-
-
 }
