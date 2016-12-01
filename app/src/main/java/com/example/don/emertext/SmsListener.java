b2b7c40c6de9eecbb9dd7012c2550c426386e3fa
gerard <gbonner689@gmail.com>
2016-12-01 12:58:54 +0000

    Move inital setup button

--- app/src/main/java/com/example/don/emertext/SmsListener.java
+++ app/src/main/java/com/example/don/emertext/SmsListener.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
 import android.telephony.SmsMessage;
 
 public class SmsListener extends BroadcastReceiver
@@ -23,7 +24,7 @@ public class SmsListener extends BroadcastReceiver
                     context.getString(R.string.personal_details_file), Context.MODE_PRIVATE).getString(context.getString(R.string.emergency_service_number_key), context.getString(R.string.default_emergency_number));
             String body = SMessage.getMessageBody().toString();
 
-            if (sender.equals(referenceNumber)) {
+            if (PhoneNumberUtils.compare(context, sender, referenceNumber)) {
                 Intent in = new Intent("SmsMessage.intent.EMERGENCY").
                         putExtra("get_msg", body);
 
