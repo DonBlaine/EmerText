f948df8451da2c22ad5b470cce7a9d17723dac7e
akhilesh <akhilesh.sharma@ucdconnect.ie>
2016-12-01 12:40:31 +0000

    removing timer

--- app/src/main/java/com/example/don/emertext/MessageScreenInteraction.java
+++ app/src/main/java/com/example/don/emertext/MessageScreenInteraction.java
@@ -25,8 +25,6 @@ public class MessageScreenInteraction extends AppCompatActivity {
     String recmsg;
     String lastmessage;
     private BroadcastReceiver mIntentReceiver;
-    Long sentTime, receiveTime;
-    Boolean recievedMessageTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,13 +83,13 @@ public class MessageScreenInteraction extends AppCompatActivity {
         msgText.setText(message);
         sendSMS(findViewById(android.R.id.content).getRootView());
 
+
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
-        recievedMessageTime = false;
         IntentFilter intentFilter = new IntentFilter("SmsMessage.intent.EMERGENCY");
         mIntentReceiver = new BroadcastReceiver() {
             @Override
@@ -112,8 +110,7 @@ public class MessageScreenInteraction extends AppCompatActivity {
     }
 
     public void sendSMS(View view) {
-        sentTime = System.currentTimeMillis();
-        recievedMessageTime = true;
+
         SmsManager text = SmsManager.getDefault();
         String message = "";
         TextView q = (TextView) findViewById(R.id.messageText);
@@ -158,15 +155,6 @@ public class MessageScreenInteraction extends AppCompatActivity {
         ll.addView(nmsg);
         scroll.fullScroll(View.FOCUS_DOWN);
         msgText.setText("");
-
-        while (recievedMessageTime){
-                receiveTime = System.currentTimeMillis();
-                if (receiveTime-sentTime > 180000){
-                    sendSMS(findViewById(android.R.id.content).getRootView());
-
-                }
-        }
-
 
     }
 
